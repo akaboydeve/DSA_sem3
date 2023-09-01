5590b8b3160e3d21e10219ecd73e2134c67ccc98
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2023-09-01 23:00:58 +0530

    Create linklist1.c

---
+++ linklist1.c
@@ -0,0 +1,17 @@
+/*wap to insert node at beginning on linklist
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node next;
+};
+
+int main()
+{
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = 50;
+    temp->next = NULL;
+}
