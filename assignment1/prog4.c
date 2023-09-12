2f1b8868776156c0ce8fe6e8ba1f548772b7a26c
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2023-09-12 22:52:19 +0530

    Create prog4.c

---
+++ assignment1/prog4.c
@@ -0,0 +1,19 @@
+/*Given a singly linked list, write a function to swap elements pairwise
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void traverseLinkList()
+{
+}
+
+int main()
+{
+    return 0;
+}
