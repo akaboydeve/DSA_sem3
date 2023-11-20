7a7a578a4048a33d39b5c67f1ca15358fdb6da16
akaboydeve <akashsardaryt@gmail.com>
2023-11-20 20:16:15 +0530

    lab 10

---
+++ lab10/la10_01.c
@@ -0,0 +1,146 @@
+/* Write a program to create a binary search tree of n data elements using linked list and
+perform the following menu driven operations
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node *insert(struct Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return createNode(data);
+    }
+    else
+    {
+        if (data < root->data)
+        {
+            root->left = insert(root->left, data);
+        }
+        else
+        {
+            root->right = insert(root->right, data);
+        }
+        return root;
+    }
+}
+
+void preorder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void inorder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void postorder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+struct Node *search(struct Node *root, int key)
+{
+    if (root == NULL || root->data == key)
+    {
+        return root;
+    }
+    if (key < root->data)
+    {
+        return search(root->left, key);
+    }
+    return search(root->right, key);
+}
+
+int main()
+{
+    struct Node *root = NULL;
+    int choice, data, key;
+
+    do
+    {
+        printf("\nBinary Search Tree Operations\n");
+        printf("1. Insert\n");
+        printf("2. Preorder Traversal\n");
+        printf("3. Inorder Traversal\n");
+        printf("4. Postorder Traversal\n");
+        printf("5. Search\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter data to insert: ");
+            scanf("%d", &data);
+            root = insert(root, data);
+            break;
+        case 2:
+            printf("Preorder Traversal: ");
+            preorder(root);
+            printf("\n");
+            break;
+        case 3:
+            printf("Inorder Traversal: ");
+            inorder(root);
+            printf("\n");
+            break;
+        case 4:
+            printf("Postorder Traversal: ");
+            postorder(root);
+            printf("\n");
+            break;
+        case 5:
+            printf("Enter element to search: ");
+            scanf("%d", &key);
+            if (search(root, key) != NULL)
+            {
+                printf("Element found\n");
+            }
+            else
+            {
+                printf("Element not found\n");
+            }
+            break;
+        case 6:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("Invalid choice\n");
+        }
+    } while (choice != 6);
+
+    return 0;
+}
